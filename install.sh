275c33b3cb1eedd80a0018042364b6831bfca475
IamThilina <333thilina@gmail.com>
2017-01-21 23:38:28 +0530

    added docker,kurento

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-##### UBUNTU 16 #########
+##### UBUNTU 16.04 #########
 
 sudo apt-get update
 
@@ -7,9 +7,12 @@ sudo apt-get update
 #git
 sudo apt-get install git
 
-#node
+#node & npm
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs
+
+#bower
+sudo npm install -g bower
 
 #LAMP
 
@@ -20,11 +23,31 @@ sudo apt-get install mysql-server
 ## php
 sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
 
+#kurento media server
+echo "deb http://ubuntu.kurento.org xenial kms6" | sudo tee /etc/apt/sources.list.d/kurento.list
+wget -O - http://ubuntu.kurento.org/kurento.gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install kurento-media-server-6.0
+
+#docker
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-get install -y docker-engine
+
+#docker-compose
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+
 
 ##############################
 
 
 #### UTILITIES ###############
+
+#node http server
+sudo npm install http-server -g
 
 #unity-tweak
 sudo apt-get install unity-tweak-tool
@@ -56,5 +79,11 @@ sudo apt-get install youtube-dl
 sudo add-apt-repository ppa:transmissionbt/ppa
 sudo apt-get update
 sudo apt-get install transmission-cli transmission-common transmission-daemon
+
+#sticky notes
+sudo add-apt-repository ppa:umang/indicator-stickynotes
+sudo apt-get update
+sudo apt-get install indicator-stickynotes
+
 
 ##############################
